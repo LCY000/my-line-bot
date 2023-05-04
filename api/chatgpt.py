25b64e88fd8670a435ab006bc72303e9c0f77a3e
LCY000 <84628528+LCY000@users.noreply.github.com>
2023-05-04 16:14:11 +0800

    Update chatgpt.py

--- api/chatgpt.py
+++ api/chatgpt.py
@@ -3,7 +3,7 @@ import os
 
 def chatgpt(input):
     # 設定OpenAI API密鑰
-    openai.api_key = "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"
+    openai.api_key = "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"
 
     # 載入ChatGPT模型
     model_engine = "text-davinci-003"
@@ -12,14 +12,14 @@ def chatgpt(input):
     input_text = input
 
     # 設定生成的文本長度
-    # output_length = 300
+    output_length = 300
 
     # 生成回應
     # response = model.complete(prompt, max_tokens=100)
     response = openai.Completion.create(
         engine=model_engine,
         prompt=input_text,
-        # max_tokens=output_length,
+        max_tokens=output_length,
     )
 
     # 輸出回應
