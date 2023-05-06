3394fffa350775d033e740636a1af681f081f199
LCY000 <84628528+LCY000@users.noreply.github.com>
2023-05-07 02:07:57 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -21,7 +21,7 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 model_engine = "text-davinci-003"
 
 # 設定生成的文本長度
-output_length = request.args.get('length', 300)
+# output_length = request.args.get('length', 300)
 
 # 建立一个字典，用来储存每个Line用户的对话
 user_dialogues = defaultdict(list)
@@ -29,39 +29,45 @@ user_dialogues = defaultdict(list)
 # 用一个字典来记录用户最后一次发送的消息的序号
 last_message_indices = defaultdict(int)
 
+# 定义每批次对话历史的最大长度
+MAX_HISTORY_LENGTH = 10
+
 def chatgpt(input_text, user_id):
     
     # 取得用户的对话历史
     dialogues = user_dialogues[user_id]
     
-    # 如果对话数量超过 30，就只保留最近的 30 个对话
-    if len(dialogues) > 30:
-        dialogues = dialogues[-30:]
+    # 如果对话数量超过最大长度，就进行分批处理
+    num_batches = len(dialogues) // MAX_HISTORY_LENGTH
+    if len(dialogues) % MAX_HISTORY_LENGTH != 0:
+        num_batches += 1
+    batched_dialogues = [dialogues[i:i+MAX_HISTORY_LENGTH] for i in range(0, len(dialogues), MAX_HISTORY_LENGTH)]
     
     # 用一个变量来记录用户最后一次发送的消息是否为偶数
     last_message_is_even = last_message_indices[user_id] % 2 == 0
     
     # 将历史对话转成 OpenAI API 所需的格式，即显示谁说的
     history_formatted = ""
-    for i, dialogue in enumerate(dialogues):
-        # 根据用户最后一次发送的消息是奇数还是偶数来决定是用户说的话还是AI说的话
-        if i % 2 == 0:
-            if last_message_is_even:
-                history_formatted += f"\nUser: {dialogue}"
+    for batch in batched_dialogues:
+        for i, dialogue in enumerate(batch):
+            # 根据用户最后一次发送的消息是奇数还是偶数来决定是用户说的话还是AI说的话
+            if i % 2 == 0:
+                if last_message_is_even:
+                    history_formatted += f"\nUser: {dialogue}"
+                else:
+                    history_formatted += f"\nAI: {dialogue}"
             else:
-                history_formatted += f"\nAI: {dialogue}"
-        else:
-            if last_message_is_even:
-                history_formatted += f"\nAI: {dialogue}"
-            else:
-                history_formatted += f"\nUser: {dialogue}"
+                if last_message_is_even:
+                    history_formatted += f"\nAI: {dialogue}"
+                else:
+                    history_formatted += f"\nUser: {dialogue}"
 
     # 生成回应
     response = openai.Completion.create(
         engine=model_engine,
         prompt=input_text,
         additional_text=history_formatted,
-        max_tokens=output_length,
+        # max_tokens=output_length,
         temperature=1.0
     )
 
