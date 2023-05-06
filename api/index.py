1e1647f23f76241e46092b9177030d82024d1dbd
LCY000 <84628528+LCY000@users.noreply.github.com>
2023-05-07 02:13:38 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -68,7 +68,7 @@ def chatgpt(input_text, user_id):
         prompt=input_text,
         additional_text=history_formatted,
         # max_tokens=output_length,
-        temperature=1.15
+        temperature=0.8
     )
 
     # 将这次的输入和回应加入用户的对话历史中
