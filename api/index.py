a93337406052c8539fefb44a243942216c60484b
LCY000 <84628528+LCY000@users.noreply.github.com>
2023-05-07 01:06:10 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -21,27 +21,36 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 model_engine = "text-davinci-003"
 
 # 設定生成的文本長度
-output_length = 300
+# output_length = 300
 
 # 建立一個字典，用來儲存每個Line用戶的前4個對話
 user_dialogues = defaultdict(list)
 
 def chatgpt(input_text, user_id):
     
-    # 取得使用者的前4個的輸入，並將它們合併為一個字串
-    history = "\n".join(user_dialogues[user_id][-4:])
+    # 取得使用者的對話歷史
+    history = user_dialogues[user_id]
+
+    # 將歷史對話轉成 OpenAI API 所需的格式，即顯示誰說的
+    history_formatted = ""
+    for i, dialogue in enumerate(history):
+        if i % 2 == 0:
+            history_formatted += f"\nUser: {dialogue}"
+        else:
+            history_formatted += f"\nAI: {dialogue}"
 
     # 生成回應
     response = openai.Completion.create(
         engine=model_engine,
         prompt=input_text,
-        additional_text=history,
-        max_tokens=output_length,
+        additional_text=history_formatted,
+        # max_tokens=output_length,
         temperature=1.3
     )
 
-    # 將這次的輸入加入使用者的對話歷史中
+    # 將這次的輸入和回應加入使用者的對話歷史中
     user_dialogues[user_id].append(input_text)
+    user_dialogues[user_id].append(response.choices[0].text)
 
     # 輸出回應
     return response.choices[0].text
